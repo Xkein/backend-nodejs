3fdbbe044e90b9245c5eaf086a09765f6e743b4d
Xkein <739502808@qq.com>
2025-03-16 02:26:11 +0800

    try fix compile error

--- windows_trybuild_32.cmd
+++ windows_trybuild_32.cmd
@@ -11,7 +11,7 @@ echo =====[ Patching Node.js ]=====
 @REM node %WORKSPACE%\node-script\do-gitpatch.js -p %WORKSPACE%\patchs\win_build_v%VERSION%.patch
 @REM node %WORKSPACE%\node-script\do-gitpatch.js -p %WORKSPACE%\patchs\lib_uv_add_on_watcher_queue_updated_v%VERSION%.patch
 copy /y %WORKSPACE%\zlib.def deps\zlib\win32\zlib.def
-node %~dp0\node-script\add_arraybuffer_new_without_stl.js deps/v8
+@REM node %~dp0\node-script\add_arraybuffer_new_without_stl.js deps/v8
 node %~dp0\node-script\make_v8_inspector_export.js
 
 echo =====[ Building Node.js ]=====
