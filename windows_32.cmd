10b49c18b27dfeedf90986e3dc0ef29b40932659
Xkein <739502808@qq.com>
2025-03-20 14:50:11 +0800

    copy files after build win32

--- windows_32.cmd
+++ windows_32.cmd
@@ -8,3 +8,13 @@ md puerts-node\nodejs\Lib\Win32\
 copy node\out\Release\libnode.dll .\puerts-node\nodejs\Lib\Win32\
 copy node\out\Release\libnode.exp .\puerts-node\nodejs\Lib\Win32\
 copy node\out\Release\libnode.lib .\puerts-node\nodejs\Lib\Win32\
+copy node\out\Release\libnode.pdb .\puerts-node\nodejs\Lib\Win32\
+
+copy node\src\node.h ..\puerts-node\nodejs\include\
+copy node\src\node_version.h ..\puerts-node\nodejs\include\
+copy node\src\node_api.h ..\puerts-node\nodejs\include\
+copy node\src\node_api_types.h ..\puerts-node\nodejs\include\
+copy node\src\js_native_api.h ..\puerts-node\nodejs\include\
+copy node\src\js_native_api_types.h ..\puerts-node\nodejs\include\
+xcopy /E /Y node\deps\uv\include ..\puerts-node\nodejs\deps\uv\
+xcopy /E /Y node\deps\v8\include ..\puerts-node\nodejs\deps\v8\
